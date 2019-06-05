62d03474179084b345697dc363e87f92b7041ff5
Danylo Kazymyrov <danil.kazim.99@gmail.com>
2019-06-05 10:12:45 +0300

    add NulPoinerException catching

--- lab6/src/com/karpenko/Model/Triangle.java
+++ lab6/src/com/karpenko/Model/Triangle.java
@@ -10,23 +10,29 @@ public class Triangle {
     public double getSideA() {
         return sideA;
     }
+
     public void setSideA(double sideA) {
         this.sideA = sideA;
     }
+
     public double getB() {
         return sideB;
     }
+
     public void setB(double b) {
         this.sideB = b;
     }
+
     public double getSideC() {
         return sideC;
     }
+
     public void setSideC(float sideC) {
         this.sideC = sideC;
     }
 
-    public Triangle(double a, double b, double c) throws InvalidTriangleMetricsException {
+    public Triangle(double a, double b, double c) throws IllegalArgumentException, InvalidTriangleMetricsException {
+        a = (int) a / 0;
         if (a <= 0)
             throw new IllegalArgumentException("Invalid size of triangle side. Side of the triangle must be greater than zero.");
 
